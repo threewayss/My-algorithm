912c4b26e304a9ef967f3c248697ef25394a7176
threewayss <way_0810@qq.com>
2025-10-17 03:08:35 +0800

    Uncomment code examples for number theory algorithms

--- 数论1.cpp
+++ 数论1.cpp
@@ -14,28 +14,53 @@
 */
 //分解质因数
 /*
- int n;
- for(int i=2;i<=n/i;i++){
-    
- }
-
-
-
-
+ void solve()
+{
+    int a;
+    cin>>a;
+    for(int i=2;i<=a/i;i++){
+        if(a%i==0){
+            int s=0;
+            while(a%i==0){
+                a/=i;
+                s++;
+            }
+            cout<<i<<" "<<s<<endl;
+        }
+    }
+    if(a>1) cout<<a<<" "<<1<<endl;
+    cout<<endl;
+}
 
 */
 //埃拉托斯特尼筛法
 /*
-
-
-
-
-
-
+    int n,cnt=0;
+    cin>>n;
+    for(int i=2;i<=n;i++){
+        if(!str[i]){
+            prime[cnt++]=i;
+            for(int j=i+i;j<=n;j+=i) str[j]=1;
+        }
+    }
+    cout<<cnt<<endl;
 */
 //线性筛（欧拉筛）
 /*
-
+    int n;
+    cin>>n;
+    for(int i=2;i<=n;i++){
+        if(!str[i]){
+            prime[cnt++]=i;
+        }
+        for(int j=0;prime[j]<=n/i;j++){
+            str[prime[j]*i]=1;
+            if(i%prime[j]==0) break;
+        }
+    }
+    cout<<cnt<<endl;
+    
+*/
 
 
 
